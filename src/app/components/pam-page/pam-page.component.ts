437c785bcfa8bdbf5b1947499951a9658fe6f5b7
hemanth567 <hemanth.b567@gmail.com>
2023-06-01 08:08:27 -0400

    select product and add to cart

--- src/app/components/pam-page/pam-page.component.ts
+++ src/app/components/pam-page/pam-page.component.ts
@@ -2,6 +2,12 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 declare var paypal: any;
 
+interface PaymentPlan {
+  type: string;
+  price: number;
+  selected: boolean;
+}
+
 @Component({
   selector: 'app-pam-page',
   templateUrl: './pam-page.component.html',
@@ -10,29 +16,47 @@ declare var paypal: any;
 export class PamPageComponent implements OnInit, AfterViewInit {
 
   @ViewChild('paypalRef', { static: true }) private paypalRef: ElementRef;
-  paymentPlans: any[] = [
-    { type: 'Plan Type 1', price: 9.99 },
-    { type: 'Plan Type 2', price: 14.99 },
-    { type: 'Plan Type 3', price: 19.99 },
-    { type: 'Plan Type 4', price: 24.99 }
+  paymentPlans: PaymentPlan[] = [
+    { type: 'Product  1', price: 9.99, selected: false },
+    { type: 'Product  2', price: 14.99, selected: false },
+    { type: 'Product 3', price: 19.99, selected: false }
   ];
+  cartItems: PaymentPlan[] = [];
+  purchasedProducts: any[] = [];
 
   constructor() {
   }
+
   ngAfterViewInit(): void {
   }
 
   ngOnInit(): void {
   }
 
-  buyNow(paypalButton, plan) {
+  toggleProductSelection(plan: PaymentPlan) {
+    plan.selected = !plan.selected;
+    if (plan.selected) {
+      this.cartItems.push(plan);
+    } else {
+      const index = this.cartItems.findIndex(item => item.type === plan.type);
+      if (index !== -1) {
+        this.cartItems.splice(index, 1);
+      }
+    }
+  }
+
+  isProductSelected(plan: PaymentPlan): boolean {
+    return plan.selected;
+  }
+
+  buyNow(paypalButton) {
     paypal.Buttons({
       createOrder: (data: any, actions: any) => {
         return actions.order.create({
           purchase_units: [
             {
               amount: {
-                value: plan.price
+                value: this.calculateTotal()
               }
             }
           ]
@@ -40,8 +64,10 @@ export class PamPageComponent implements OnInit, AfterViewInit {
       },
       onApprove: (data: any, actions: any) => {
         return actions.order.capture().then((details: any) => {
-          // Handle successful payment
-          console.log(details);
+          this.purchasedProducts = this.cartItems.slice();
+          console.log("paypal callback object", details);
+          console.log("purchased products", this.purchasedProducts);
+          this.cartItems.length = 0;
         });
       },
       onCancel: (data: any) => {
@@ -53,6 +79,16 @@ export class PamPageComponent implements OnInit, AfterViewInit {
       fundingSource: paypal.FUNDING.PAYPAL
     }).render(paypalButton);
 
+  }
+
+
+  calculateTotal(): number {
+    return this.cartItems.reduce((total, item) => total + item.price, 0);
+  }
+
+  viewCart() {
+
+    console.log('View Cart');
   }
 
 }
